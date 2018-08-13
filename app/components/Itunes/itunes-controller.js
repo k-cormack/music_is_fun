c8b7652e33dcbccb52c83c273f75a33950f07a1a
k-cormack <k.cormack@gmail.com>
2018-08-13 01:18:58 -0600

    flexbox, css, html changes

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -17,27 +17,41 @@ function drawSongs(results) {
   for (let i = 0; i < results.length; i++) {
     const artist = results[i];
     template += `
+
+    <div class="card-deck">
+      <div class="card border-dark mb-3">
+        <img class="card-img-top img-responsive" src="${artist.albumArt}" alt="art"/>
+          <div class="card-body">
+            <h5 class="card-title">${artist.title}</h5>
+            <p class="card-text">${artist.artist}</p>
+            <p class="card-text">${artist.collection}</p>
+            <p class="card-text">$${artist.price}</p>
+          </div>
+          <div class="card-footer">
+            <audio controls class="audio-controls">
+                <source src="${artist.preview}" type="audio/mpeg" />
+            </audio>
+          </div>
+      </div>
+    </div>
     
-        <div class="col-sm-3" style="outline: 1px solid black">
-          <p>${artist.title}</p>
-          <div class="album-art">
-            <img src="${artist.albumArt}" alt="art" class="img-responsive"/>
-          </div>
-          <p>${artist.artist}</p>
-          <p>${artist.collection}</p>
-          <p>${artist.price}</p>
-          <div>
-            <audio controls  class="audio-controls">
-              <source src="${artist.preview}" type="audio/mpeg"/>
-            </audio>       
-          </div>
-        </div>     
-          `      
             
+          `
+
   }
   document.getElementById("song-list").innerHTML = template
   // @ts-ignore
   document.getElementById("artistForm").reset()
+
+  document.addEventListener("play", function (playing) {
+    var song = document.getElementsByTagName("audio");
+    for (var i = 0, songLength = song.length; i < songLength; i++) {
+      if (song[i] != playing.target) {
+        song[i].pause();
+      }
+    }
+  },
+    true);
 
 
   // clearArtist()
@@ -66,3 +80,19 @@ class ItunesController {
 
 
 export default ItunesController
+
+
+  // < div class="col-sm-3" style = "outline: 1px solid black" >
+  //   <p>${artist.title}</p>
+  //     <div class="album-art">
+  //       <img src="${artist.albumArt}" alt="art" class="img-responsive" />
+  //     </div>
+  //   <p>${artist.artist}</p>
+  //   <p class="album-name">${artist.collection}</p>
+  //   <p>${artist.price}</p>
+  //     <div class="row">
+  //       <audio controls class="audio-controls">
+  //         <source src="${artist.preview}" type="audio/mpeg" />
+  //       </audio>
+  //     </div>
+  // </div > 
